a0f0bfca47c59fa866f4a35126ce0f09fc889552
Velengio Deriksen Charles <velengio@gmail.com>
2026-05-19 20:08:59 +0700

    add: sumpah lupa ini apaan

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -27,8 +27,9 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
+            'message' => 'Login successful',
             'access_token' => $token,
             'token_type' => 'Bearer',
-        ]);
+        ], 200);
     }
 }
